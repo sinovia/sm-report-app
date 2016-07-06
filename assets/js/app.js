b0b4388b0d456c8a59c017c6bcd016ff2ca952ce
SINOVIA <somayfield@gmail.com>
2016-07-05 23:49:51 -0400

    Pre Submission Push

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,16 +6,27 @@ SMapp.config(function($routeProvider) {
   //Consumer View Routes
     .when('/home', {
       templateUrl: 'templates/home.html',
-      // controller: 'ReportController'
+      controller: 'ReportController'
     })
     .when('/report-list', {
       templateUrl: 'templates/report-list.html',
-      // controller: 'ReportController'
+      controller: 'ReportController'
     })
+
+    .when('/showreport', {
+      templateUrl: 'templates/show-report.html',
+      controller: 'ReportbyDayController'
+      })
+
     .when('/showreport:reportid', {
       templateUrl: 'templates/show-report.html',
-      // controller: 'ReportController'
+      controller: 'ReportbyDayController'
       })
+
+      .when('/addreport', {
+        templateUrl: 'templates/add-report.html',
+        controller: 'ReportController'
+        })
       .otherwise({
         redirectTo: "/home"
 
@@ -37,9 +48,36 @@ SMapp.controller('ReportbyDayController', ['$scope', '$http', '$routeParams', fu
   var url = "http://localhost:3007/reports"
      $http.get(url).success( function(data) {
         $scope.reportList = data;
-        $scope.whichReport = $routeParams.reportID;
+        $scope.whichReport = $routeParams.reportid;
+        var thisone = $routeParams.reportid
+
 
         console.log(data)
+        console.log($scope.whichReport + " is the reportid");
      });
    }]
 );
+
+SMapp.controller('AdminController', function($scope, $http){
+
+  $('.saveReport').click(function (e) {
+    console.log("clicked");
+
+    $scope.newReport = ({
+
+      "ShowName" : $('input[name="saveShowName"]').val(),
+      "RehearsalDate": $('input[name="saveRehearsalDate"]').val(),
+      "SM": $('input[name="saveSM"]').val(),
+      "Director": $('input[name="saveDirector"]').val(),
+      "Props": $('input[name="saveProps"]').val(),
+      "Costumes": $('input[name="saveCostumes"]').val(),
+      "Lights": $('input[name="saveLights"]').val(),
+      "Sound": $('input[name="saveSound"]').val(),
+      "RunTime": $('input[name="saveRunTime"]').val(),
+        "General": $('input[name="saveGeneral"]').val(),
+    });
+    $http.post("http://localhost:3007/reports", $scope.newReport).success(function(data) {
+    console.log(data);
+  });
+});
+});
